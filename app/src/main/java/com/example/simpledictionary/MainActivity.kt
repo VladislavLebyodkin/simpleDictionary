8573366e1a9e2c0dd41d5c0b0a45b3b809b84381
Vladislav <forojoo@gmail.com>
2021-01-26 15:33:10 +0700

    Splash screen was added

--- app/src/main/java/com/example/simpledictionary/MainActivity.kt
+++ app/src/main/java/com/example/simpledictionary/MainActivity.kt
@@ -13,11 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_SimpleDictionary)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-
     }
 }
 
